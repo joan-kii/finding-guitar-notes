58da79e5ddffba0146484f65d1d036510b82935d
joan-kii <joan.sb81@gmail.com>
2021-02-27 18:12:01 +0100

    añade estilos SelectGame

--- src/components/SelectGame.js
+++ src/components/SelectGame.js
@@ -1,31 +1,53 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import Typography from '@material-ui/core/Typography';
+import Note from '../assets/note.jpg';
+import Chord from '../assets/chord.jpg';
 
+// Buttons Selection
 const images = [
   {
-    url: '../assets/chord.jpg',
+    url: Note,
     title: 'Notes',
-    width: '50%'
+    width: 300
   },
   {
-    url: '../assets/chord.jpg',
+    url: Chord,
     title: 'Chords',
-    width: '50%'
+    width: 300
   }
 ];
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
+    justifyContent: 'center',
+    marginTop: '10vh',
+  },
+  paper: {
+    display: 'flex',
+    position: 'absolute',
+    height: '30%',
+    width: '30%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: '10vh',
+  },
+  buttons: {
+    display: 'flex',
+    position: 'absolute',
     flexWrap: 'wrap',
-    minWidth: 300,
-    width: '100%',
+    minWidth: 200,
+    width: 'auto',
+    justifyContent: 'center',
   },
   image: {
     position: 'relative',
     height: 200,
+    border: '2px solid white',
     [theme.breakpoints.down('xs')]: {
       width: '100% !important',
       height: 100,
@@ -33,17 +55,16 @@ const useStyles = makeStyles((theme) => ({
     '&:hover, &$focusVisible': {
       zIndex: 1,
       '& $imageBackdrop': {
-        opacity: 0.15,
+        opacity: 0.1,
       },
       '& $imageMarked': {
         opacity: 0,
       },
       '& $imageTitle': {
-        border: '4px solid currentColor',
+        border: '2px solid currentColor',
       },
     },
   },
-  focusVisible: {},
   imageButton: {
     position: 'absolute',
     left: 0,
@@ -55,13 +76,13 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'center',
     color: theme.palette.common.white,
   },
-  imageScr: {
+  imageSrc: {
     position: 'absolute',
     left: 0,
     right: 0,
-    top: 0, 
+    top: 0,
     bottom: 0,
-    backgroundSize: 'cover',
+    backgroundSize: 300,
     backgroundPosition: 'center 40%',
   },
   imageBackdrop: {
@@ -77,6 +98,7 @@ const useStyles = makeStyles((theme) => ({
   imageTitle: {
     position: 'relative',
     padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) + 6}px`,
+    fontSize: 22,
   },
   imageMarked: {
     height: 3,
@@ -94,38 +116,46 @@ const SelectGame = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      {images.map((image) => (
-        <ButtonBase 
-          focusRipple
-          key={image.title}
-          className={classes.image}
-          focusVisibleClassName={classes.focusVisible}
-          style={{
-            width: image.width,
-          }}
-        >
-          <span 
-            className={classes.imageScr}
+    <Container fixed className={classes.root}>
+      <Paper className={classes.paper} elevation={3}>
+        <Typography>
+          Welcome to Finding Guitar Notes!
+        </Typography>
+      </Paper>
+      <div className={classes.buttons}>
+        {images.map((image) => (
+          <ButtonBase 
+            focusRipple
+            key={image.title}
+            className={classes.image}
+            focusVisibleClassName={classes.focusVisible}
             style={{
-              backgroundImage: `url(${image.url})`,
+              width: image.width,
+              margin: 20,
             }}
-          />
-          <span className={classes.imageBackdrop} />
-          <span className={classes.imageButton}>
-            <Typography 
-              component='span'
-              variant='subtitle1'
-              color='inherit'
-              className={classes.imageTitle}
-            >
-              {image.title}
-              <span className={classes.imageMarked} />
-            </Typography>
-          </span>
-        </ButtonBase>
-      ))}
-    </div>
+          >
+            <span 
+              className={classes.imageSrc}
+              style={{
+                backgroundImage: `url(${image.url})`,
+              }}
+            />
+            <span className={classes.imageBackdrop} />
+            <span className={classes.imageButton}>
+              <Typography 
+                component='span'
+                variant='subtitle1'
+                color='inherit'
+                className={classes.imageTitle}
+              >
+                {image.title}
+                <span className={classes.imageMarked} />
+              </Typography>
+            </span>
+          </ButtonBase>
+        ))}
+      </div>
+    </ Container>
   );
 }; 
 
