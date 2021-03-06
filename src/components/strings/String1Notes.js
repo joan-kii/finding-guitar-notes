804965349dcb2ec5d8d84a1f32600919bc623f36
joan-kii <joan.sb81@gmail.com>
2021-03-06 06:13:46 +0100

    añade menu item dinámico. bug render notas

--- src/components/strings/String1Notes.js
+++ src/components/strings/String1Notes.js
@@ -1,38 +1,72 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
+import { ExercisesContext } from '../Exercises';
 import Infozone from '../Infozone';
 import { createFretboard } from '../../modules/createFretboard';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const fretboard = createFretboard();
 const string1 = fretboard[0].props.children;
 
-const handleClick = (event) => {
-  console.log(event.target);
-};
-
-for (let fret of string1) {
-  const newFret = React.cloneElement(fret, 
-    {className: `${[fret.props.className]} clickable`, 
-    onClick: handleClick, 
-    'aria-controls': 'simple-menu', 
-    'aria-haspopup': 'true'}, null);
-  if (fret.key === '13') {
-    const newLastFret = React.createElement('div', {key: fret.key, className: 'note-fret clickable'}, 
-      React.createElement('div', {key: fret.key, className: 'double-fretmark'}, ''));
-    string1.splice(fret.key - 1, 1, newLastFret); 
-  } else {
-    string1.splice(fret.key - 1, 1, newFret);
-  }
-}
-
 const String1Notes = () => {
-  
+
+  const { exercises, setExercises } = useContext(ExercisesContext);
+  const notesString1 = ['E', 'F', 'F#-Gb', 'G', 'G#-Ab', 'A', 'A#-Bb', 'B', 'C', 'C#-Db', 'D', 'D#-Eb', 'e'];
   const [String1Fretboard, setString1Fretboard] = useState(fretboard);
   const [newString1, setNewString1] = useState(string1);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const handleClick = (event) => {
+    setAnchorEl(event.target);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const optionNotes = (anchorEl) => {
+    if (anchorEl) {
+      const options = [anchorEl.id];
+      options.push(notesString1[Math.floor(Math.random()* 14)]);
+      options.push(notesString1[Math.floor(Math.random()* 14)]);
+      return options;
+    } else {
+      return [];
+    }
+  };
+
+  // Create the exercise fretboard
+
+  for (let fret of string1) {
+    const newFret = React.cloneElement(fret, 
+      {id: notesString1[string1.indexOf(fret)], className: `${[fret.props.className]} clickable`, 
+      onClick: handleClick, 
+      'aria-controls': 'simple-menu', 
+      'aria-haspopup': 'true'}, null);
+    if (fret.key === '13') {
+      const newLastFret = React.createElement('div', {id: notesString1[string1.indexOf(fret)], key: fret.key, className: 'note-fret clickable', onClick: handleClick}, 
+        React.createElement('div', {key: fret.key, className: 'double-fretmark'}, ''));
+      string1.splice(string1.indexOf(fret), 1, newLastFret); 
+    } else {
+      string1.splice(string1.indexOf(fret), 1, newFret);
+    }
+  }
 
   return (
     <div>
       <div className='fretboard'>
         {String1Fretboard}
+        <Menu 
+          id='simple-menu'
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose} >
+          {optionNotes(anchorEl).map((note, index) => {
+            <MenuItem 
+              key={note}
+              onClick={(event) => handleClick(event, note)}>
+                {note}
+            </MenuItem>
+          })}
+        </Menu>
       </div>
       <Infozone />
     </div>
